64856603930e02c4f89b562654b4af6c00b3a83e
Will Szabó <100345587+will-szabo@users.noreply.github.com>
2022-06-02 14:40:35 +0200

    read the file "/etc/resolv.conf"

--- examenopdracht.py
+++ examenopdracht.py
@@ -2,3 +2,6 @@ defgateway = $(ip route)
 ip -j -p = $uitvoerCommandoIp
 json.loads(uitvoerCommandoIp)
 
+configParser = ConfigParser.RawConfigParser()
+leesConfigFile = r'/etc/resolv.conf '
+configParser.read(leesConfigFile)
